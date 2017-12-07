417654d96939557745fccedb5b82707dbde64cb8
Dylan Fox <foxishdabest@gmail.com>
2017-12-07 09:57:15 -0800

    Update index.php

--- chat/index.php
+++ chat/index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <link rel="stylesheet" href="/css/chatroom.css">
   <title>Chat | 2D4U</title>
   </head>
   <body>
-    
+    <div id="chatbox">
+      <p>Welcome, <b>user</b>!</p>
+    </div>
   </body>
 </html>
