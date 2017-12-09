f717a7fe910dec33f8a9d07831da1e84fac0290e
Dylan Fox <foxishdabest@gmail.com>
2017-12-09 13:14:50 -0800

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -14,12 +14,6 @@ var drawModule = (function () {
         ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
   }
 
-  var scoreText = function() {
-    var score_text = "Score: " + score;
-    ctx.fillStyle = 'black';
-    ctx.fillText(score_text, 145, h-18);
-  }
-
   var drawSnake = function() {
       var length = 4;
       snake = [];
@@ -51,7 +45,6 @@ var drawModule = (function () {
       if (snakeX == -1 || snakeX == w/snakeSize || snakeY == -1 || snakeY == h/snakeSize || checkCollision(snakeX, snakeY, snake)) {
           //restart game
           btn.removeAttribute('disabled', true);
-          score = 0;
           ctx.clearRect(0,0,w,h);
           gameloop = clearInterval(gameloop);
           return;          
