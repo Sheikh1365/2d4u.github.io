c5569f2b83455f5cf75658042351d1ee7bc29c50
Dylan Fox <foxishdabest@gmail.com>
2017-12-10 13:36:50 -0800

    Update obstacle.js

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,13 +1,1 @@
-var obstacle = function(x, y) {
-  ctx.fillStyle = 'darkblue';
-  ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
-  ctx.fillStyle = 'lightblue';
-  ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
-}
 
-var createObstacle = function() {
-      obstacle = {
-        x: Math.floor((Math.random() * 30) + 1),
-        y: Math.floor((Math.random() * 30) + 1)
-      }
-  }
