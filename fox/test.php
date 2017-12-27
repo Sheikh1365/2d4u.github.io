f185bd459daac31b80e22a8187367853d70bf96c
Dylan Fox <foxishdabest@gmail.com>
2017-12-27 13:21:33 -0800

    Create test.php

---
+++ fox/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<?php
+$visitor = $_SERVER['REMOTE_ADDR'];
+if (preg_match("/73.170.172.81/",$visitor)) {
+      header('Location: https://2d4u.github.io/fox');
+} else {
+      header('Location: https://2d4u.github.io/');
+};
+?>
+</html>
